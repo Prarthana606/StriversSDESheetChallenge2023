e232917dd5cddac1395bd54521b6da111a7ae833
Prarthana606 <132823806+Prarthana606@users.noreply.github.com>
2023-06-15 19:05:19 +0530

    Kadane.cpp

--- Kadane.cpp
+++ Kadane.cpp
@@ -1,12 +1,6 @@
 #include <bits/stdc++.h>
 long long maxSubarraySum(int arr[], int n)
 {
-    /*
-        Don't write main().
-        Don't read input, it is passed as function argument.
-        No need to print anything.
-        Taking input and printing output is handled automatically.
-    */
   long long int sum=0;
   long long int maxsum=LONG_MIN;
   for(int i=0;i<n;i++)
